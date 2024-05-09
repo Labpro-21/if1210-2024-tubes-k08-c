b4ee9ef36c5322aae1b426cff4e1a735707dadbb
Bukananda <Bukansayawoi@gmail.com>
2024-05-09 21:16:48 +0700

    fix: Perubahan jenis seed lcg

--- src/lcg.py
+++ src/lcg.py
@@ -1,9 +1,8 @@
-seed_list = [0] # Lakukan pengambilan data dari save_load
+import time
 
 # Fungsi Linear Congruential Generator
 def lcg(modulus,a,c,seed):
     seed = (a * seed + c) % modulus
-    seed_list.append(seed)
     # Menyimpan seed_list pada save_load
     return seed
 
@@ -11,7 +10,7 @@ def lcg(modulus,a,c,seed):
 def random_n(n): # n merupakan jumlah digit
     hasil = ""
     for i in range(n):
-        seed = lcg(2**64, 6364136223846793005, 1442695040888963407, seed_list[len(seed_list)-1])
+        seed = lcg(2**64, 6364136223846793005, 1442695040888963407, time.time_ns()*(i+19283))
         string_seed = str(seed)
         hasil += string_seed[len(string_seed)//2]
 
@@ -24,3 +23,5 @@ def randint1(n1,n2):
         hasil = random_n(digit)
         if n1 <= hasil <= n2:
             return hasil
+        
+print(randint1(10,100))
