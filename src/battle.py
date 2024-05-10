0ebe419157d3dea3c636163219470c77dfd16720
Bukananda <Bukansayawoi@gmail.com>
2024-05-10 09:57:06 +0700

    feat: Penambahan aksi Attack

--- src/battle.py
+++ src/battle.py
@@ -22,6 +22,13 @@ def add_oc(id,add):
     
     tabel.dict_to_csv(file_path,user)
 
+def name_user(id):
+    file_path = os.path.join("data/","init/","user.csv")
+    user = load.csv_to_dict(file_path)
+    for i in user:
+        if i['id'] == str(id):
+            return i['username']
+
 def battle(id):
     monster_player = load.csv_to_dict(os.path.join("data/","init/","monster_inventory.csv"))
     monster_dict = load.csv_to_dict(os.path.join("data/","init/","monster.csv"))
@@ -80,28 +87,63 @@ Level     : {level_monster}
          |  |   |   |
           \\._\\   \\._\\ 
 
-RAWRRR, Agent X mengeluarkan monster {monster_n(pilihan-1)['type']} !!!
+RAWRRR, Agent {name_user(id)} mengeluarkan monster {monster_n(pilihan-1)['type']} !!!
+
+Name : {monster_n(pilihan-1)['type']}
+ATK Power : {list_player_monster[0]}
+DEF Power : {list_player_monster[1]}
+HP        : {list_player_monster[2]}
+Level     : {list_monster_user[pilihan-1]['level']}
+""")
+
+    turn_num = 1
+    while True:
+        print(f"""============ TURN {turn_num} {monster_n(pilihan-1)['type']} ============
+              
+1. Attack
+2. Use Potion
+3. Quit
+""")
+            
+        pilihan_2 = int(input("Pilih perintah: "))
+        if pilihan_2 == 1:
+            monster.attack(skill_monster_enemy,int(list_player_monster[0]))
+
+            if skill_monster_enemy[2] <= 0:
+                skill_monster_enemy[2] = 0
+            
+            print(f"""\n{monster_n(pilihan-1)['type']},  menyerang {type_monster['type']} !!!
+
+Name      : {type_monster['type']}
+ATK Power : {skill_monster_enemy[0]}
+DEF Power : {skill_monster_enemy[1]}
+HP        : {skill_monster_enemy[2]}
+Level     : {level_monster}
+""")
+            if skill_monster_enemy[2] <= 0:
+                print(f"Selamat, Anda berhasil mengalahkan monster {type_monster['type']} !!!")
+                add_oc_coin = lcg.randint1(5*level_monster,30*level_monster)
+                add_oc(id,add_oc_coin)
+                print(f"Total OC yang diperoleh: {add_oc_coin}")
+                break
+            else:
+                monster.attack(list_player_monster,skill_monster_enemy[0])
+                if list_player_monster[2] <= 0:
+                    list_player_monster[2] = 0
+                print(f"""SCHWINKKK, {type_monster['type']} menyerang {monster_n(pilihan-1)['type']} !!!
 
 Name      : {monster_n(pilihan-1)['type']}
 ATK Power : {list_player_monster[0]}
 DEF Power : {list_player_monster[1]}
 HP        : {list_player_monster[2]}
 Level     : {list_monster_user[pilihan-1]['level']}
-
-============ TURN 1 {monster_n(pilihan-1)['type']} ============
-1. Attack
-2. Use Potion
-3. Quit
-
 """)
-    
-    pilihan_2 = int(input("Pilih perintah: "))
-    if pilihan_2 == 1:
-        print(f"{monster_n(pilihan-1)['type']},  menyerang Zuko !!!")
-        monster.attack(skill_monster_enemy,list_monster_user[0])
-        if list_player_monster[2] <= 0:
-            print("Selamat, Anda berhasil mengalahkan monster Zuko !!!")
-            add_oc_coin = lcg.randint1(5*level_monster,30*level_monster)
-            add_oc(id,add_oc_coin)
-            print(f"Total OC yang diperoleh: {add_oc_coin}")
-        else:
+                if list_player_monster[2] <= 0:
+                    print("Yahhh, Anda dikalahkan monster Zuko. Jangan menyerah, coba lagi !!!")
+                    break
+                else:
+                    turn_num += 1
+
+        elif pilihan_2 == 2:
+            # fungsi pada potion
+            pass
