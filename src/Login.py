fe0e5dfc305473724a13b74a4388904e52433c00
spectrespectre0 <williamadt123@gmail.com>
2024-05-16 15:57:57 +0700

    fix : overhaul fix of login func
    login has now been overhauled and fixed

--- src/Login.py
+++ src/Login.py
@@ -1,40 +1,53 @@
 # Kamus
-def isFind(x,array,key): #output tuple(bool,integer) -> (ada atau tidak,index jika ada jika tidak -9999 )
+
+# search function
+def is_find(searched: str, user_array: list[dict], key: str) -> (bool, int):
+    # output tuple(bool,integer) -> (ada atau tidak,index jika ada jika tidak -9999 )
     cond = False
     index = -9999
-    for i in range(len(array)):
-        if x == array[i][key]:
-            cond=True
+    for i in range(len(user_array)):
+        if searched == user_array[i][key]:
+            cond = True
             index = i
-    return (cond,index)
+    return cond, index
+
 
 # Algoritma Utama
-def login(listUser):
+def login(list_user: list[dict]):
     # List of dictionary -> [{'id':1,'username':aaaa,'password':bbbbbb,'role':agen,'oc':0}]
-    # listUser : list of tuple -> [('id',[1,2,3,...]),('username',[...]),('password',[....]),('role',[...]),('oc',[...])]
-    user = input("Username: ")
-    pw = input("Password: ")
+    # listUser : list of tuple -> [('id',[1,2,3,...]),
+    # ('username',[...]),('password',[....]),('role',[...]),('oc',[...])]
+    while True:
+        print("Login into the system !!!")
+        user = input("Username: ")
+        pw = input("Password: ")
 
-    id = -9999
-    isLogin = False
-    index= isFind(pw,listUser,'password')[1]
+        user_id = -9999
+        is_login = False
+        user_exists, index = is_find(user, list_user, 'username')
 
-    if isFind(user,listUser,'username')[0]:
-        if isFind(pw,listUser,'password')[0]:
-            print("""
-Selamat datang, Agent Purry!
-Masukkan command “help” untuk daftar command yang dapat kamu panggil.""")
-            id = listUser[index]['id']
-            isLogin = True
+        if user_exists:
+            if list_user[index]['password'] == pw:
+                print(f"""
+    Welcome back, Agent {user}!
+    Enter “HELP” command to show all available actions.""")
+                user_id = list_user[index]['id']
+                is_login = True
+                return user_id, is_login
+            else:
+                print("Wrong password!")
+
         else:
-            print("""
-Password salah!""")
-            
-    else:
-        print("""
-Username tidak terdaftar!""")
-    return(id,isLogin)
-    # Aku gk tau id perlu apa tidak ntar kalau memang tidak perlu di fix kan nanti aja
+            print("Username is not registered!")
+            while True:
+                register_move = input("Do you want to register instead? Y/N : ")
+                if register_move == "Y":
+                    return -999, False
+                elif register_move == "N":
+                    break
+                else:
+                    print("Enter a valid option")
+
 
 # TESTING
 # arr= [{'id': '1', 'username': 'Mr_Monogram', 'password': 'monogrammer77', 'role': 'admin'}, {'id': '2', 'username': 'Asep_Spakbor', 'password': 'asepwow123', 'role': 'agent'}, {'id': '3', 'username': 'Agen_P', 'password': 'platypus123', 'role': 'agent'}, {'id': '4', 'username': 'B4ngk1dd0ssss', 'password': 'bangkitganteng', 'role': 'agent'}, {'id': '5', 'username': 'Kenny_agen_rahasia', 'password': 'kribogeming55', 'role': 'agent'}]
