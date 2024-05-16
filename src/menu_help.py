2e8d721b593b119428064637be7abbd83a4dc8b7
spectrespectre0 <williamadt123@gmail.com>
2024-05-16 16:46:52 +0700

    fix : changed how the menu&help func works
    menu&help now acts only as a help displayer. Any function is called directly from main

--- src/menu_help.py
+++ src/menu_help.py
@@ -1,46 +1,30 @@
-def help(status,username):
-    if status == "non-agent":
-        print("""=========== HELP ===========
-Kamu belum login sebagai role apapun. Silahkan login terlebih dahulu.
-
-1. LOGIN: Masuk ke akun yang sudah ada
-2. REGISTER: Buat akun baru
-3. EXIT: Keluar dari game
-""")
-        input_help = input()
-        # fungsi selanjutnya
-        # buat fungsi ketika input invalid
-    
-    elif status == "agent":
+def help_ui(status: str, username:str):
+    if status == "agent":
         print(f"""=========== HELP ===========
 
-Halo Agent {username}. Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu tidak sesat kemudian. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:
+Hello Agent {username}. Since you requested HELP, here are your available actions:
 
-1. LOGOUT: Keluar dari akun
-2. SHOP: Melakukan pembelian di toko
-3. MONSTER: Melihat owca-dex yang dimiliki oleh Agent
-4. LABORATORY: Melakukan upgrade monster yang dimiliki di inventory 
-5. BATTLE: Melakukan pertarungan dengan musuh 
-6. SAVE: Melakukan penyimpanan data
-7. EXIT: Keluar dari game 
+1. LOGOUT: Log out from your account
+2. SHOP: Visit the Shop to purchase items or monsters
+3. MONSTER: View your OWCA-Dex to see owned monsters
+4. LAB: Upgrade the monsters you own
+5. BATTLE: Engage in battle with enemies
+6. ARENA: Fight hordes of monsters in the Arena
+7. GACHA: Play and win lots of OWCA Coins !!! 
+8. SAVE: Save current progress and data
+9. EXIT: Exit the game
 """)
-        input_help = input()
-        # fungsi selanjutnya
-        # buat fungsi ketika input invalid
     
     elif status == "admin":
         print("""=========== HELP ===========
 
-Selamat datang, Admin. Berikut adalah hal-hal yang dapat kamu lakukan:
+Welcome back, Admin. Since you requested HELP, here are your available actions:
 
-1. LOGOUT: Keluar dari akun yang sedang digunakan
-2. SHOP: Melakukan manajemen pada SHOP sebagai tempat jual beli peralatan Agent
-3. MONSTER: Melakukan manajemen penambahan/pengurangan monster
-4. SAVE: Melakukan penyimpanan data
-5. EXIT: Keluar dari game
+1. LOGOUT: Log out from your account
+2. SHOP: Manage the SHOP stock for agents to buy from
+3. MONSTER: Add or Remove monsters from the database
+4. SAVE: Save current progress and data
+5. EXIT: Exit the game
 """)
-        input_help = input()
-        # fungsi selanjutnya
-        # buat fungsi ketika input invalid
 
-help("agent")
+
