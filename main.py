5365ca56a69d92d109c423414e504bf87f24dcc9
spectrespectre0 <williamadt123@gmail.com>
2024-05-16 15:58:40 +0700

    feat : function implementations
    login, register, exit functions now properly implemented

--- main.py
+++ main.py
@@ -9,16 +9,27 @@ import src.load as load
 import src.save as save
 import src.ascii_art as art
 import src.jackpot as gacha
+import src.register as register
+import src.login as login
+import src.exit_kill as exit_kill
 
-
-
-users_data, monster_data, inv_item_data, inv_monster_data, shop_item_data, shop_monster_data = load.load_files()
-
-
-print(monster_data)
+user_data, monster_data, inv_item_data, inv_monster_data, shop_item_data, shop_monster_data = load.load_files()
+print("Before entering system, please Login/Register")
+logged_in = False
+while not logged_in:
+    login_choice = input("LOGIN / REGISTER : ").upper()
+    if login_choice == "LOGIN":
+        logged_id, logged_in = login.login(user_data)
+    elif login_choice == "REGISTER":
+        logged_id, logged_in, inv_monster_data = register.register_ui(user_data, monster_data, inv_monster_data)
+    else:
+        print("choose a valid option")
 while True:
-    choice = input("next action (save/gacha) testing : ")
-    if choice == "save":
-        save.save(users_data, monster_data, inv_item_data, inv_monster_data, shop_item_data, shop_monster_data)
-    elif choice == "gacha":
+    choice = input("next action (SAVE/GACHA/EXIT) testing : ").upper()
+    if choice == "SAVE":
+        save.save(user_data, monster_data, inv_item_data, inv_monster_data, shop_item_data, shop_monster_data)
+    elif choice == "GACHA":
         gacha.gacha(1, 250, monster_data, inv_monster_data)
+    elif choice == "EXIT":
+        exit_kill.exit_kill(user_data, monster_data, inv_item_data, inv_monster_data, shop_item_data, shop_monster_data)
+        break
